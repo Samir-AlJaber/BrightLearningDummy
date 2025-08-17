b8318cc31be86aa8308b4b386d3022f994b0855d
Tanimm27 <ktanim647@gmail.com>
2025-08-17 21:45:25 +0600

    sign up page added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage/HomePage";
-import LoginPage from "./Pages/LoginPage/LoginPage"; // Import LoginPage
+import LoginPage from "./Pages/LoginPage/LoginPage";
+import SignUpPage from "./Pages/signup/signup"; // ছোট হাতের নাম ব্যবহার করো
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} /> {/* Add this */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} /> {/* signup page */}
       </Routes>
     </Router>
   );

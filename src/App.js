6993e5ab50f00006c96770fe9f2502cf447b6c33
Fahim Raiyan <fahim.cse.20220204098@aust.edu>
2025-08-17 21:12:31 +0600

    Raiyan

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage/HomePage";
+import LoginPage from "./Pages/LoginPage/LoginPage"; // Import LoginPage
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {}
+        <Route path="/login" element={<LoginPage />} /> {/* Add this */}
       </Routes>
     </Router>
   );
